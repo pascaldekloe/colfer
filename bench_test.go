bb91d286fff3d032dbbd8ac3bed53e273175329c
Pascal S. de Kloe <pascal@quies.net>
2016-03-10 14:20:18 +0100

    Compare with Protocol Buffers.

--- bench_test.go
+++ bench_test.go
@@ -12,16 +12,14 @@ import (
 
 //go:generate go run ./cmd/colf/main.go -p testdata go testdata/bench/scheme.colf
 //go:generate go run ./cmd/colf/main.go -p testdata java testdata/bench/scheme.colf
+//go:generate protoc --gogofaster_out=. -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gogo/protobuf/protobuf testdata/bench/scheme.proto
 
-var rnd = rand.New(rand.NewSource(time.Now().Unix()))
+var testSet = make([]*bench.Colfer, 1000)
 
-func generate(n int) ([]*bench.Colfer, [][]byte, int64) {
-	objects := make([]*bench.Colfer, n)
-	serials := make([][]byte, n)
-	size := 0
-
-	typ := reflect.TypeOf(objects).Elem()
-	for i := range objects {
+func init()  {
+	rnd := rand.New(rand.NewSource(time.Now().Unix()))
+	typ := reflect.TypeOf(testSet).Elem()
+	for i := range testSet {
 		v, ok := quick.Value(typ, rnd)
 		if !ok {
 			panic("can't generate Bench values")
@@ -35,38 +33,102 @@ func generate(n int) ([]*bench.Colfer, [][]byte, int64) {
 			o = new(bench.Colfer)
 		}
 
-		b := o.Marshal(make([]byte, 1000))
-
-		objects[i], serials[i] = o, b
-		size += len(b)
+		testSet[i] = o
 	}
-
-	return objects, serials, int64(size / n)
 }
+
+var holdData []byte
 
 func BenchmarkEncode(b *testing.B) {
-	n := 1000
-	objects, _, avgSize := generate(n)
-	buf := make([]byte, 1000)
+	buf := make([]byte, 512)
 
-	b.SetBytes(avgSize)
 	b.ReportAllocs()
 	b.ResetTimer()
-
 	for i := b.N; i != 0; i-- {
-		objects[rnd.Intn(n)].Marshal(buf)
+		holdData = testSet[i%len(testSet)].Marshal(buf)
 	}
 }
 
-func BenchmarkDecode(b *testing.B) {
-	n := 1000
-	_, serials, avgSize := generate(n)
+func BenchmarkEncodeProtoBuf(b *testing.B) {
+	buf := make([]byte, 512)
+	protoBufSet := make([]*bench.ProtoBuf, len(testSet))
+	for i, o := range testSet {
+		protoBufSet[i] = &bench.ProtoBuf{
+			Key: o.Key,
+			Host: o.Host,
+			Addr: o.Addr,
+			Port: o.Port,
+			Size_: o.Size,
+			Hash: o.Hash,
+			Ratio: o.Ratio,
+			Route: o.Route,
+		}
+	}
 
-	b.SetBytes(avgSize)
 	b.ReportAllocs()
 	b.ResetTimer()
-
 	for i := b.N; i != 0; i-- {
-		new(bench.Colfer).Unmarshal(serials[rnd.Intn(n)])
+		i, err := protoBufSet[i%len(protoBufSet)].MarshalTo(buf)
+		if err != nil {
+			b.Error(err)
+		}
+		holdData = buf[:i]
+	}
+}
+
+var holdColfer = new(bench.Colfer)
+
+func BenchmarkDecode(b *testing.B) {
+	serials := make([][]byte, len(testSet))
+	for i, o := range testSet {
+		serials[i] = o.Marshal(make([]byte, 1024))
+	}
+
+	zero := new(bench.Colfer)
+	buf := new(bench.Colfer)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := b.N; i != 0; i-- {
+		*buf = *zero	// init
+		buf.Unmarshal(serials[i%len(serials)])
+		*holdColfer = *buf
+	}
+}
+
+var holdProtoBuf = new(bench.ProtoBuf)
+
+func BenchmarkDecodeProtoBuf(b *testing.B) {
+	serials := make([][]byte, len(testSet))
+	for i, o := range testSet {
+		p := &bench.ProtoBuf{
+			Key: o.Key,
+			Host: o.Host,
+			Addr: o.Addr,
+			Port: o.Port,
+			Size_: o.Size,
+			Hash: o.Hash,
+			Ratio: o.Ratio,
+			Route: o.Route,
+		}
+		var err error
+		serials[i], err = p.Marshal()
+		if err != nil {
+			b.Error(err)
+		}
+	}
+
+	zero := new(bench.ProtoBuf)
+	buf := new(bench.ProtoBuf)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := b.N; i != 0; i-- {
+		*buf = *zero	// init
+		holdProtoBuf := new(bench.ProtoBuf)
+		if err := buf.Unmarshal(serials[i%len(serials)]); err != nil {
+			b.Error(err)
+		}
+		*holdProtoBuf = *buf
 	}
 }
