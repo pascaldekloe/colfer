143442ccdc456849fd4c70888e094a7595d02f55
Pascal S. de Kloe <pascal@quies.net>
2016-12-11 01:59:56 +0100

    Modest QUinit test report.

--- ecma/run.js
+++ ecma/run.js
@@ -1,5 +1,15 @@
 var testrunner = require("qunit");
 
+testrunner.setup({
+	log: {
+		errors: true,
+		tests: true,
+		coverage: true
+	},
+	coverage: true,
+	maxBlockDuration: 2000
+});
+
 testrunner.run({
 	code: "./Colfer.js",
 	tests: "./test.js"
