4e82d5cc5caa447606de25f3feeec62260a5e961
Pascal S. de Kloe <pascal@quies.net>
2016-12-11 13:49:33 +0100

    FIX Go benchmark build with Makefile and vendored package paths.

--- go/bench/bench_test.go
+++ go/bench/bench_test.go
@@ -4,22 +4,16 @@ import (
 	"testing"
 
 	flatbuffers "github.com/google/flatbuffers/go"
-
-	"github.com/pascaldekloe/colfer/testdata/bench/build/gen"
 )
 
-//go:generate $GOPATH/bin/colf -b build -f go scheme.colf
-//go:generate protoc --gogofaster_out=build/gen -I. -I${GOPATH}/src -I${GOPATH}/src/github.com/gogo/protobuf/protobuf scheme.proto
-//go:generate flatc -o build -g scheme.fbs
-
-var testData = []*gen.Colfer{
+var testData = []*Colfer{
 	{Key: 1234567890, Host: "db003lz12", Port: 389, Size: 452, Hash: 0x488b5c2428488918, Ratio: 0.99, Route: true},
 	{Key: 1234567891, Host: "localhost", Port: 22, Size: 4096, Hash: 0x243048899c24c824, Ratio: 0.20, Route: false},
 	{Key: 1234567892, Host: "kdc.local", Port: 88, Size: 1984, Hash: 0x000048891c24485c, Ratio: 0.06, Route: false},
 	{Key: 1234567893, Host: "vhost8.dmz.example.com", Port: 27017, Size: 59741, Hash: 0x5c2408488b9c2489, Ratio: 0.0, Route: true},
 }
 
-var protoTestData = []*gen.ProtoBuf{
+var protoTestData = []*ProtoBuf{
 	{Key: testData[0].Key, Host: testData[0].Host, Port: uint32(testData[0].Port), Size_: testData[0].Size, Hash: testData[0].Hash, Ratio: testData[0].Ratio, Route: testData[0].Route},
 	{Key: testData[1].Key, Host: testData[1].Host, Port: uint32(testData[1].Port), Size_: testData[1].Size, Hash: testData[1].Hash, Ratio: testData[1].Ratio, Route: testData[1].Route},
 	{Key: testData[2].Key, Host: testData[2].Host, Port: uint32(testData[2].Port), Size_: testData[2].Size, Hash: testData[2].Hash, Ratio: testData[2].Ratio, Route: testData[2].Route},
@@ -50,19 +44,19 @@ func init() {
 	for i, o := range testData {
 		builder := flatbuffers.NewBuilder(0)
 		host := builder.CreateString(o.Host)
-		gen.FlatBuffersStart(builder)
-		gen.FlatBuffersAddKey(builder, o.Key)
-		gen.FlatBuffersAddHost(builder, host)
-		gen.FlatBuffersAddPort(builder, o.Port)
-		gen.FlatBuffersAddSize(builder, o.Size)
-		gen.FlatBuffersAddHash(builder, o.Hash)
-		gen.FlatBuffersAddRatio(builder, o.Ratio)
+		FlatBuffersStart(builder)
+		FlatBuffersAddKey(builder, o.Key)
+		FlatBuffersAddHost(builder, host)
+		FlatBuffersAddPort(builder, o.Port)
+		FlatBuffersAddSize(builder, o.Size)
+		FlatBuffersAddHash(builder, o.Hash)
+		FlatBuffersAddRatio(builder, o.Ratio)
 		if o.Route {
-			gen.FlatBuffersAddRoute(builder, 1)
+			FlatBuffersAddRoute(builder, 1)
 		} else {
-			gen.FlatBuffersAddRoute(builder, 0)
+			FlatBuffersAddRoute(builder, 0)
 		}
-		builder.Finish(gen.FlatBuffersEnd(builder))
+		builder.Finish(FlatBuffersEnd(builder))
 		flatSerials[i] = builder.FinishedBytes()
 	}
 }
@@ -70,8 +64,8 @@ func init() {
 // prevent compiler optimization
 var (
 	holdSerial       []byte
-	holdData         *gen.Colfer
-	holdProtoBufData *gen.ProtoBuf
+	holdData         *Colfer
+	holdProtoBufData *ProtoBuf
 )
 
 func BenchmarkMarshal(b *testing.B) {
@@ -104,19 +98,19 @@ func BenchmarkMarshal(b *testing.B) {
 
 			builder := flatbuffers.NewBuilder(0)
 			host := builder.CreateString(o.Host)
-			gen.FlatBuffersStart(builder)
-			gen.FlatBuffersAddKey(builder, o.Key)
-			gen.FlatBuffersAddHost(builder, host)
-			gen.FlatBuffersAddPort(builder, o.Port)
-			gen.FlatBuffersAddSize(builder, o.Size)
-			gen.FlatBuffersAddHash(builder, o.Hash)
-			gen.FlatBuffersAddRatio(builder, o.Ratio)
+			FlatBuffersStart(builder)
+			FlatBuffersAddKey(builder, o.Key)
+			FlatBuffersAddHost(builder, host)
+			FlatBuffersAddPort(builder, o.Port)
+			FlatBuffersAddSize(builder, o.Size)
+			FlatBuffersAddHash(builder, o.Hash)
+			FlatBuffersAddRatio(builder, o.Ratio)
 			if o.Route {
-				gen.FlatBuffersAddRoute(builder, 1)
+				FlatBuffersAddRoute(builder, 1)
 			} else {
-				gen.FlatBuffersAddRoute(builder, 0)
+				FlatBuffersAddRoute(builder, 0)
 			}
-			builder.Finish(gen.FlatBuffersEnd(builder))
+			builder.Finish(FlatBuffersEnd(builder))
 
 			holdSerial = builder.Bytes[builder.Head():]
 		}
@@ -127,7 +121,7 @@ func BenchmarkUnmarshal(b *testing.B) {
 	b.Run("colfer", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := b.N; i > 0; i-- {
-			o := new(gen.Colfer)
+			o := new(Colfer)
 			holdData = o
 
 			_, err := o.Unmarshal(colferSerials[i%len(colferSerials)])
@@ -140,7 +134,7 @@ func BenchmarkUnmarshal(b *testing.B) {
 	b.Run("protobuf", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := b.N; i > 0; i-- {
-			o := new(gen.ProtoBuf)
+			o := new(ProtoBuf)
 			holdProtoBufData = o
 
 			err := o.Unmarshal(protoSerials[i%len(protoSerials)])
@@ -153,11 +147,11 @@ func BenchmarkUnmarshal(b *testing.B) {
 	b.Run("flatbuf", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := b.N; i > 0; i-- {
-			o := new(gen.Colfer)
+			o := new(Colfer)
 			holdData = o
 
 			bytes := flatSerials[i%len(flatSerials)]
-			buf := new(gen.FlatBuffers)
+			buf := new(FlatBuffers)
 			buf.Init(bytes, flatbuffers.GetUOffsetT(bytes))
 			o.Key = buf.Key()
 			o.Host = string(buf.Host())
@@ -171,7 +165,7 @@ func BenchmarkUnmarshal(b *testing.B) {
 }
 
 func BenchmarkMarshalReuse(b *testing.B) {
-	buf := make([]byte, gen.ColferSizeMax)
+	buf := make([]byte, ColferSizeMax)
 
 	b.Run("colfer", func(b *testing.B) {
 		b.ReportAllocs()
@@ -215,32 +209,32 @@ func BenchmarkMarshalReuse(b *testing.B) {
 
 			builder.Reset()
 			host := builder.CreateString(o.Host)
-			gen.FlatBuffersStart(builder)
-			gen.FlatBuffersAddKey(builder, o.Key)
-			gen.FlatBuffersAddHost(builder, host)
-			gen.FlatBuffersAddPort(builder, o.Port)
-			gen.FlatBuffersAddSize(builder, o.Size)
-			gen.FlatBuffersAddHash(builder, o.Hash)
-			gen.FlatBuffersAddRatio(builder, o.Ratio)
+			FlatBuffersStart(builder)
+			FlatBuffersAddKey(builder, o.Key)
+			FlatBuffersAddHost(builder, host)
+			FlatBuffersAddPort(builder, o.Port)
+			FlatBuffersAddSize(builder, o.Size)
+			FlatBuffersAddHash(builder, o.Hash)
+			FlatBuffersAddRatio(builder, o.Ratio)
 			if o.Route {
-				gen.FlatBuffersAddRoute(builder, 1)
+				FlatBuffersAddRoute(builder, 1)
 			} else {
-				gen.FlatBuffersAddRoute(builder, 0)
+				FlatBuffersAddRoute(builder, 0)
 			}
-			builder.Finish(gen.FlatBuffersEnd(builder))
+			builder.Finish(FlatBuffersEnd(builder))
 			holdSerial = builder.Bytes[builder.Head():]
 		}
 	})
 }
 
 func BenchmarkUnmarshalReuse(b *testing.B) {
-	holdData = new(gen.Colfer)
-	holdProtoBufData = new(gen.ProtoBuf)
+	holdData = new(Colfer)
+	holdProtoBufData = new(ProtoBuf)
 
 	b.Run("colfer", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := b.N; i > 0; i-- {
-			*holdData = gen.Colfer{}
+			*holdData = Colfer{}
 			_, err := holdData.Unmarshal(colferSerials[i%len(colferSerials)])
 			if err != nil {
 				b.Fatal(err)
@@ -251,7 +245,7 @@ func BenchmarkUnmarshalReuse(b *testing.B) {
 	b.Run("protobuf", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := b.N; i > 0; i-- {
-			*holdProtoBufData = gen.ProtoBuf{}
+			*holdProtoBufData = ProtoBuf{}
 			err := holdProtoBufData.Unmarshal(protoSerials[i%len(protoSerials)])
 			if err != nil {
 				b.Fatal(err)
@@ -259,14 +253,14 @@ func BenchmarkUnmarshalReuse(b *testing.B) {
 		}
 	})
 
-	buf := new(gen.FlatBuffers)
+	buf := new(FlatBuffers)
 
 	b.Run("flatbuf", func(b *testing.B) {
 		b.ReportAllocs()
 		for i := b.N; i > 0; i-- {
 			bytes := flatSerials[i%len(flatSerials)]
 			buf.Init(bytes, flatbuffers.GetUOffsetT(bytes))
-			*holdData = gen.Colfer{}
+			*holdData = Colfer{}
 			holdData.Key = buf.Key()
 			holdData.Host = string(buf.Host())
 			holdData.Port = buf.Port()
